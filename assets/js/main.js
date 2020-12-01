84ad2cf8a182d3da0be00a8604ae3d0d14e6bceb
salvoab <salvatore.abate85@gmail.com>
2020-12-01 15:06:28 +0100

    Funzioni per generare la sequenza di numeri casuali con test

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,22 @@
  * Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 ********************************************************************************************************/
 
+function getRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomSequence(min, max, sequenceLength){
+    var sequence = [];
+    var number;
+    while(sequence.length !== sequenceLength){
+        number = getRandomNumber(min, max);
+        if( !sequence.includes(number)){
+            sequence.push(number);
+        }
+    }
+    return sequence;
+}
+
+//test
+var testArray = getRandomSequence(20,100,8);
+console.log(testArray);
