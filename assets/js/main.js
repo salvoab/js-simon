84c414e498e146b50e5a456724292f2f8cf3421f
salvoab <salvatore.abate85@gmail.com>
2020-12-01 14:55:31 +0100

    Cartelle e file di base

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/******************************************************************************************************
+ * Esporre all'utente 5 numeri casuali diversi.
+ * Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+ * Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+********************************************************************************************************/
+
