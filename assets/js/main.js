3b2925c0ea598ba9a939bbbc4e21b7bc1d8689c9
salvoab <salvatore.abate85@gmail.com>
2020-12-01 15:32:04 +0100

    Funzione per mostrare i numeri e timeout per cancellarli
    In fase di test il timeout è settato a 5 secondi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 /******************************************************************************************************
+ * Simon Says
  * Esporre all'utente 5 numeri casuali diversi.
  * Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
  * Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
@@ -20,6 +21,21 @@ function getRandomSequence(min, max, sequenceLength){
     return sequence;
 }
 
-//test
-var testArray = getRandomSequence(20,100,8);
-console.log(testArray);
+var saimonSaysSequence = getRandomSequence(1, 50, 5);
+var elencoIndovinati = [];
+
+$(function(){
+    function showNumbers(numbers){
+        var message = $('#message');
+        for(var i = 0; i<numbers.length; i++){
+            message.append(numbers[i] + " " );
+        }
+    }
+    showNumbers(saimonSaysSequence);
+
+    setTimeout(function () {
+        //Cancello i numeri mostrati all'utente
+        $('#message').text(" ");
+        //TO-DO Chiedere all'utente i numeri che si ricorda
+    }, 5000);
+});
